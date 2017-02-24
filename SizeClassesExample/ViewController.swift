e82cfc79ebafbbcf4f2c8c8dc8d1691529ff975c
Junior Samaroo <jns0987654321@gmail.com>
2017-02-23 22:54:41 -0500

    Initial Commit

---
+++ SizeClassesExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClassesExample
+//
+//  Created by Junior Samaroo on 2017-02-23.
+//  Copyright © 2017 Junior Samaroo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
